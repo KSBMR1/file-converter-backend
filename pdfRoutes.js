8ec8a20aa8bc3ee9fbaeafe0fe09813e89b0d85b
KSBMR1 <shahbazrafi2003@gmail.com>
2025-05-14 14:27:43 +0600

    Delete pdfRoutes.js

--- pdfRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { pdfToWord } = require('../controllers/pdfController');
-
-router.post('/to-word', pdfToWord);
-
-module.exports = router;
