efb7c3e51957e2c692dd0dd6b1f6ffbeb3090414
KSBMR1 <shahbazrafi2003@gmail.com>
2025-05-14 14:27:06 +0600

    Delete imageRoutes.js

--- imageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { jpgToPng } = require('../controllers/imageController');
-
-router.post('/jpg-to-png', jpgToPng);
-
-module.exports = router;
